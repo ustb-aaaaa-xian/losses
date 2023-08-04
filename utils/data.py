bd0577221db7d872ba3e5972d2be238ecbd536c1
段长贤 <duancx@nb-ai.com>
2023-08-04 09:37:20 +0800

    dataprocess

--- utils/data.py
+++ utils/data.py
@@ -3,6 +3,9 @@ from PIL import Image
 import os
 import random
 
+from torchvision.transforms import transforms
+
+
 class Dataprocess(Dataset):
 	def __init__(self,data_root ="",mode = "train",transform = None,data_size = None):
 		super().__init__()
@@ -25,6 +28,7 @@ class Dataprocess(Dataset):
 		self.data_list = data_list
 		self.tf = transform
 		self.data_size = data_size
+		random.seed(1000)
 		random.shuffle(self.data_list)
 		self.mode = mode
 		if self.data_size is not None :
@@ -42,3 +46,59 @@ class Dataprocess(Dataset):
 		if self.tf is not None:
 			data = self.tf(data)
 		return data,target
+
+class Dataprocess_n(Dataset):
+	def __init__(self,data_root ="",mode = "train",transform = None,num_each_class : int = None):
+		super().__init__()
+		data_dir = ""
+		if mode == "train" or mode == "val":
+			data_dir = os.path.join(data_root,mode)
+		elif mode == "test":
+			data_dir = os.path.join(data_root,mode)
+
+		# data
+		data_dic = {}
+		data_list = []
+		classes = []
+		for sub_dir in os.listdir(data_dir):
+			if sub_dir not in data_dic:
+				data_dic[sub_dir] = []
+			classes.append(sub_dir)
+			sub_dir_path = os.path.join(data_dir,sub_dir)
+
+			for file in os.listdir(sub_dir_path):
+				file_path = os.path.join(sub_dir_path,file)
+				data_dic[sub_dir].append(file_path)
+		self.data_dic = data_dic
+		self.tf = transform
+		for target_name,files in self.data_dic.items():
+			datas = []
+			target = int(classes.index(target_name))
+			for file in files:
+				datas.append(file)
+			# 取数据
+			random.seed(1000)
+			datas = random.sample(datas,num_each_class)
+			for data in datas:
+				data_list.append((target,data))
+		self.data_list = data_list
+	def __len__(self):
+		return len(self.data_list)
+	def __getitem__(self, item):
+		target,data_path = self.data_list[item]
+		data = Image.open(data_path)
+		if self.tf is not None:
+			data = self.tf(data)
+		return data,target
+
+if __name__ == "__main__":
+	tf = transforms.Compose(
+		[
+			transforms.ToTensor(),
+			transforms.Lambda(lambda x: x.repeat(3, 1, 1)),
+			transforms.Resize((224, 224)),
+			transforms.Normalize([.5, .5, .5], [.5, .5, .5])
+		]
+	)
+	train_set = Dataprocess_n(data_root="../data/MSTAR-10",mode = "train",transform = tf,num_each_class=10)
+	print(len(train_set))
